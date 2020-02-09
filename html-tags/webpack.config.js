a84a3e8be08ca2fd096becb6a3ac31242db6d23d
Yukitaka Maeda <yumaeda@gmail.com>
2020-02-09 11:33:50 +0900

    Add rules and commandline parameters

--- html-tags/webpack.config.js
+++ html-tags/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
                 enforce: 'pre',
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: /node_modules/,
+                exclude: /node_modules/
             }
         ]
     },
